a9b07b4f3b66e48bb913b416d2cc3697856e5943
Szymon-Krysztopolski <szymon.krysztopolski@gmail.com>
2021-12-19 10:04:06 +0100

    scenario -> json + json -> scenario

--- src/main/java/pl/put/poznan/transformer/app/Application.java
+++ src/main/java/pl/put/poznan/transformer/app/Application.java
@@ -1,11 +1,11 @@
 package pl.put.poznan.transformer.app;
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.put.poznan.transformer.base.Scenario;
 import pl.put.poznan.transformer.base.Step;
 import pl.put.poznan.transformer.base.SubScenario;
+import pl.put.poznan.transformer.logic.FileReader;
 import pl.put.poznan.transformer.logic.Visitor;
 
 
@@ -24,56 +25,20 @@ public class Application {
     public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException, FileNotFoundException, JSONException {
         //SpringApplication.run(Application.class, args);
 
-        String ofile = "file11.txt";
+        String ofile = "file12.txt";
+        String fileJson="file20.json";
 
         PrintWriter output = new PrintWriter("with_steps-" + ofile);
 
         //Obiekty scenariusze
-        //Scenario x = new Scenario(ofile);
-
-        JSONObject scen = new JSONObject();
-        scen.put("Tytul", "Dodanie książki");
-        JSONArray aktorzy = new JSONArray();
-        aktorzy.put("Bibliotekarz");
-        aktorzy.put("Sekretarka");
-        aktorzy.put("Nauczyciel");
-        scen.put("Aktorzy", aktorzy);
-        scen.put("Aktor systemowy", "System");
-
-        JSONArray substep1 = new JSONArray();
-        substep1.put(new JSONObject().put("Step","Bibliotekarz wybiera opcje dodania nowej pozycji książkowej"));
-        substep1.put(new JSONObject().put("Step","Wyświetla się formularz."));
-        substep1.put(new JSONObject().put("Step","Bibliotekarz podaje dane książki."));
-
-        JSONArray substep2 = new JSONArray();
-        substep2.put(new JSONObject().put("Step","IF: Bibliotekarz pragnie dodać egzemplarze książki"));
-        substep2.put(new JSONObject().put("Step","Bibliotekarz wybiera opcję definiowania egzemplarzy"));
-        substep2.put(new JSONObject().put("Step","System prezentuje zdefiniowane egzemplarze"));
-
-        JSONArray substep3 = new JSONArray();
-        substep3.put(new JSONObject().put("Step","FOR EACH egzemplarz:"));
-        substep3.put(new JSONObject().put("Step","Bibliotekarz wybiera opcję dodania egzemplarza"));
-        substep3.put(new JSONObject().put("Step","System prosi o podanie danych egzemplarza"));
-        substep3.put(new JSONObject().put("Step","Bibliotekarz podaje dane egzemplarza i zatwierdza."));
-        substep3.put(new JSONObject().put("Step","System informuje o poprawnym dodaniu egzemplarza i prezentuje zaktualizowaną listę egzemplarzy."));
-
-        substep2.put(substep3);
-
-        substep1.put(substep2);
-
-        substep1.put(new JSONObject().put("Step","Bibliotekarz zatwierdza dodanie książki."));
-        substep1.put(new JSONObject().put("Step","System informuje o poprawnym dodaniu książki."));
-        substep1.put(new JSONObject().put("Step","Bibliotekarz zatwierdza dodanie książki."));
-        substep1.put(new JSONObject().put("Step","System informuje o poprawnym dodaniu książki."));
-
-        scen.put("Content", substep1);
+        Scenario x1 = new Scenario(ofile);
 
         FileWriter file = null;
         //private static FileWriter file;
         try {
             // Constructs a FileWriter given a file name, using the platform's default charset
-            file = new FileWriter("./files/file20.txt");
-            file.write(scen.toString());
+            file = new FileWriter("./files/"+fileJson);
+            file.write(new Gson().toJson(x1));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -84,11 +49,17 @@ public class Application {
                 file.flush();
                 file.close();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
-/*
+ 
+        File myFile = new File("files/"+fileJson);
+        Scanner myReader = new Scanner(myFile);
+
+        Gson g = new Gson();
+        Scenario x = g.fromJson(myReader.nextLine(), Scenario.class);
+
+
         System.out.println("Dane scenariusza:");
         System.out.println("Tytuł: "+x.title);
         System.out.print("Aktorzy: ");
@@ -99,7 +70,7 @@ public class Application {
         ////Wyświetlenie scenariusza
         System.out.println("\n"+"Wyświetlenie scenariusza");
         x.Scenarioshow();
-
+/*
         ////Wyświetlenie scenariusza do określonego poziomu zagłębienia (poziom główny to 1)
         System.out.println("\n"+"Scenariusz do poziomu zagłebienia");
         //x = new Scenario(ofile);
@@ -125,9 +96,9 @@ public class Application {
         ////Zapis scenariusza do pliku
         System.out.println("\n" + "Zapis");
         x.Savetofile(output);
-
+ */
         output.close();
 
- */
+
     }
 }
