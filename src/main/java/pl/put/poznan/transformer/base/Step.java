c0f2d75cd5ebfd69f5b41e55faf4cf90bf1d390e
Grabosk1 <56175973+Grabosk1@users.noreply.github.com>
2021-12-20 17:21:51 +0100

    3 klasa dokumentacji.

--- src/main/java/pl/put/poznan/transformer/base/Step.java
+++ src/main/java/pl/put/poznan/transformer/base/Step.java
@@ -2,17 +2,33 @@ package pl.put.poznan.transformer.base;
 
 import pl.put.poznan.transformer.logic.Visitor;
 
+/**
+ * Klasa Step to podstawowa klasa wykorzystywana w projekcie do pracy nad scneriuszem.
+ * Wykorzystywana przez wizytatora. Obiekt klasy Step jest odzwierciedleniem kroku scenariusza 
+ * zawierającym jego tresc oraz poziom zaglebienia. 
+ */
 public class Step {
-
+    /**
+     * Treść kroku oraz poziom zaglebienia.
+     */
     public String value;
     public int level;
 
-
+    /**
+     * Klasyczyny konstrukotr przypisujący wartości.
+     * @param a poziom zaglebienia
+     * @param tmp tresc kroku
+     */
     public Step(int a, String tmp) {
         value = tmp;
         level = a;
     }
 
+    /**
+     * Funkcja accept służy do "wpuszczenia" do obiektu instancji konkretnego wizytatora,
+     * pozwalając mu na wykonanie swojej implementacji funkcji visit.
+     * @param v konkretny obiekt wizytatora.
+     */
     public void accept(Visitor v){
 
         v.visit(this);
