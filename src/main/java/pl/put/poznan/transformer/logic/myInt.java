540dfadf3c43887c9a6ed4c699d47b357c8c8505
Grabosk1 <56175973+Grabosk1@users.noreply.github.com>
2021-12-20 16:32:35 +0100

    My Int dokumentacja

--- src/main/java/pl/put/poznan/transformer/logic/myInt.java
+++ src/main/java/pl/put/poznan/transformer/logic/myInt.java
@@ -1,19 +1,38 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Klasa MyInt wykorzystywana w celu iteracji kroków scenariusza.
+ * MyInt wykorzystywana jest w celu przesyłania wartości liczbowej przez referencje.
+ */
 public class myInt {
-    public int value;
+    private int value;
 
-    public myInt(int a){
+    /**
+     * Przypisanie wartości początkowej.
+     * @param a Przypisywana wartośc.
+     */
+    public myInt(int a) {
         value = a;
     }
 
-    public void increment(){
-        this.value+=1;
+    /**
+     * Inkrementacja value o 1.
+     */
+    public void increment() {
+        this.value += 1;
     }
-    public void reset(){
-        this.value=3;
+
+    /**
+     * Resetowanie wartości value do 3. Przypisujemy w resecie taką wartość w celu ominięcia tytułu,aktorów oraz systemaktora.
+     */
+    public void reset() {
+        this.value = 3;
     }
-    public int getValue(){
+
+    /**
+     * Zwracanie wartości value.
+     */
+    public int getValue() {
         return value;
     }
 }
