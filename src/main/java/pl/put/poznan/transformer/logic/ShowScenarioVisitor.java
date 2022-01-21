231e36a19196133d8730bfebc95591322d749285
Krzysztof Gatka <68946441+Gacioreks@users.noreply.github.com>
2022-01-21 23:14:46 +0100

    Dokumentacja

--- src/main/java/pl/put/poznan/transformer/logic/ShowScenarioVisitor.java
+++ src/main/java/pl/put/poznan/transformer/logic/ShowScenarioVisitor.java
@@ -3,13 +3,29 @@ package pl.put.poznan.transformer.logic;
 import pl.put.poznan.transformer.base.Step;
 import pl.put.poznan.transformer.base.SubScenario;
 
+
+/**
+ * Klasa ShowScenarioVisitor jest implementacja interfejsu Visitor.
+ * Sluzy do wyswietlania scenariusza.
+ */
+
 public class ShowScenarioVisitor implements Visitor{
 
+
+    /**
+     * Funkcja visit uruchamiana jest dla kazdego scenariusza, jest pusta poniewaz wymaga tego interfejs ktory implementuje.
+     * @param sc Odwiedzany podscenariusz.
+     */
     @Override
     public void visit(SubScenario sc) {
 
     }
 
+
+    /**
+     * Funkcja visit uruchamiana jest dla kazdego kroku scenariusza, wyswietla krok.
+     * @param s Odwiedzany krok.
+     */
     @Override
     public void visit(Step s) {
         StringBuilder tmp = new StringBuilder();
