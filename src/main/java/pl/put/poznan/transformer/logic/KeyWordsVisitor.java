13bd36323e24d7fa4a769040988345f6cadcdd43
Krzysztof Gatka <68946441+Gacioreks@users.noreply.github.com>
2022-01-21 23:18:37 +0100

    Dokumentacja

--- src/main/java/pl/put/poznan/transformer/logic/KeyWordsVisitor.java
+++ src/main/java/pl/put/poznan/transformer/logic/KeyWordsVisitor.java
@@ -3,15 +3,28 @@ package pl.put.poznan.transformer.logic;
 import pl.put.poznan.transformer.base.Step;
 import pl.put.poznan.transformer.base.SubScenario;
 
+/**
+ * Klasa KeyWordsVisitor jest implementacja interfejsu Visitor.
+ * Sluzy do obliczenia ilosci slow kluczowych w scenariuszu
+ */
+
 public class KeyWordsVisitor implements Visitor{
 
     private int key_words = 0;
 
+    /**
+     * Funkcja visit uruchamiana jest dla kazdego scenariusza, jest pusta poniewaz wymaga tego interfejs ktory implementuje.
+     * @param sc Odwiedzany podscenariusz.
+     */
     @Override
     public void visit(SubScenario sc) {
 
     }
 
+    /**
+     * Funkcja visit uruchamiana jest dla kazdego kroku scenariusza, zwieksza wartosc pola prywatnego key_words jesli w kroku znajduje sie slowo kluczowe
+     * @param s Odwiedzany krok.
+     */
     @Override
     public void visit(Step s) {
         String [] temp = s.value.split(" ");
@@ -23,6 +36,11 @@ public class KeyWordsVisitor implements Visitor{
         }
     }
 
+    /**
+     * Funkcja getKeyWords zwraca ilosc slow kluczowych w scenariuszu
+     * Powinna byc uruchamiana bezposrednio po wywolaniu funkcji visit na wszystkich krokach scenariusza.
+     * @return Ilosc slow kluczowych w Scenariuszu.
+     */
     public int getKeyWords(){
         System.out.println("Liczba słów kluczowych: " + this.key_words);
         //System.out.println("Zerowanie key_words wizytatora...");
