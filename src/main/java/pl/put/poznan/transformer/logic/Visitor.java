633676f55362c313afe0271c113032e2981d210b
Krzysztof Gatka <68946441+Gacioreks@users.noreply.github.com>
2022-01-21 23:20:48 +0100

    Dokumentacja

--- src/main/java/pl/put/poznan/transformer/logic/Visitor.java
+++ src/main/java/pl/put/poznan/transformer/logic/Visitor.java
@@ -3,7 +3,18 @@ package pl.put.poznan.transformer.logic;
 import pl.put.poznan.transformer.base.Step;
 import pl.put.poznan.transformer.base.SubScenario;
 
+/**
+ * Interfejs Visitor pozwala na implementacje wzorca visitor
+ */
 public interface Visitor {
+    /**
+     * Funkcja visit ktora powinna byc zaimplementowana dla kazdego visitora
+     * @param s Odwiedzany krok
+     */
     public void visit(Step s);
+    /**
+     * Funkcja visit ktora powinna byc zaimplementowana dla kazdego visitora
+     * @param sc Odwiedzany podscenariusz
+     */
     public void visit(SubScenario sc);
 }
